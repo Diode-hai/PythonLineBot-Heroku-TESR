ddfada6ced2b8527d09489e4a5aaf26aa83fc156
anoney <ponza.zaza11@gmail.com>
2018-08-16 13:45:55 +0700

    Update pythonweb_p.py

--- pythonweb_p.py
+++ pythonweb_p.py
@@ -67,7 +67,11 @@ def handle_message(event):
     	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='OFF LED1'))
 	#message = ImageSendMessage(original_content_url='https://www.img.in.th/images/04a428c1264fde336fb867412d648f40.jpg',preview_image_url='https://www.img.in.th/images/04a428c1264fde336fb867412d648f40.jpg')
 	
-	line_bot_api.reply_message(event.reply_token, message)
+	try:
+		line_bot_api.reply_message(event.reply_token, message)
+	except InvalidSignatureError:
+		abort(400)
+
 	
 	
     	#REST API NETPIE OFF LED
