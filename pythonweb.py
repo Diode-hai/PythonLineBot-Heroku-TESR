2dfb6b6f00aac25d93ab265dc7f4f1d97102d5cf
anoney <ponza.zaza11@gmail.com>
2018-08-16 09:39:52 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -70,11 +70,11 @@ def handle_message(event):
 	
     elif "menu" in str(event.message.text):
     	line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Menu'))
-	#image_message = ImageSendMessage(
-    	#original_content_url='https://previews.123rf.com/images/seamartini/seamartini1708/seamartini170800547/84922979-japanese-cuisine-restaurant-menu-template-vector-lunch-offer-for-sweet-syrup-tangerine-tory-kenko-ya.jpg',
-    	#preview_image_url='https://previews.123rf.com/images/seamartini/seamartini1708/seamartini170800547/84922979-japanese-cuisine-restaurant-menu-template-vector-lunch-offer-for-sweet-syrup-tangerine-tory-kenko-ya.jpg'
-    	#)
-	#line_bot_api.push_message('ItfoFj89IMTUAR2ERKN1yPxAPZk4UvEC4fperPkGrCg/L6GwTXKR/sOC1KEYyMJppqHG9UdSDvkWNLEWHcC5E5SfptTgPKsgcMgQzFj9nDNTaJlDhv/Xw+0ahLBCWC8nO8sMe6GSGd+dP6fmoFMPNwdB04t89/1O/w1cDnyilFU=', image_message)
+	image_message = ImageSendMessage(
+    	original_content_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg',
+    	preview_image_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg'
+    	)
+	line_bot_api.push_message('ItfoFj89IMTUAR2ERKN1yPxAPZk4UvEC4fperPkGrCg/L6GwTXKR/sOC1KEYyMJppqHG9UdSDvkWNLEWHcC5E5SfptTgPKsgcMgQzFj9nDNTaJlDhv/Xw+0ahLBCWC8nO8sMe6GSGd+dP6fmoFMPNwdB04t89/1O/w1cDnyilFU=', image_message)
 
     	#--REST API NETPIE OFF LED--
     	#r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
