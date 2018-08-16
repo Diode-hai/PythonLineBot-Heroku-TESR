ab2a4052fba2bfef8712d9eb758c5be32b6cdc2e
anoney <ponza.zaza11@gmail.com>
2018-08-16 13:52:55 +0700

    Update pythonweb_p.py

--- pythonweb_p.py
+++ pythonweb_p.py
@@ -67,9 +67,9 @@ def handle_message(event):
     	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='OFF LED1'))
 	#message = ImageSendMessage(original_content_url='https://www.img.in.th/images/04a428c1264fde336fb867412d648f40.jpg',preview_image_url='https://www.img.in.th/images/04a428c1264fde336fb867412d648f40.jpg')
 	
-	line_bot_api.send_photo(event.reply_token, photo=open(message, 'rb'))
+	
 	#try:
-		#line_bot_api.reply_message(event.reply_token, message)
+	line_bot_api.reply_message(event.reply_token, message)
 	#except InvalidSignatureError:
 		#abort(400)
 
