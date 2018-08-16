6d0cedb6ba9a918a7628e1efcce278a13d638c4c
anoney <ponza.zaza11@gmail.com>
2018-08-16 14:51:29 +0700

    Update pythonweb_image.py

--- pythonweb_image.py
+++ pythonweb_image.py
@@ -71,7 +71,7 @@ def handle_message(event):
     	r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
 
     elif "menu" in str(event.message.text):
-        line_bot_api.reply_message(event.reply_token,image_menu)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Menu pon'))
 
         #REST API NETPIE OFF LED
     	r = requests.put(url, data = {'':'MENU'} , auth=(str(KEY),str(SECRET)))
