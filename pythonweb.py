75192eab6e697a9d7fad24c69e0e5dd12052ec64
anoney <ponza.zaza11@gmail.com>
2018-08-16 10:15:59 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -32,7 +32,7 @@ url = 'https://api.netpie.io/topic/' + str(APPID) + str(Topic)
 
 urlRESTAPI = 'https://api.netpie.io/topic/' + str(APPID) + str(Topic) + '?auth=' + str(KEY) + ':' + str(SECRET)
 #https://api.netpie.io/topic/LineBotRpi/LED_Control?auth=Jk0ej35pLC7TVr1:edWzwTUkzizhlyRamWWq6nF9I
-
+message = ImageSendMessage(original_content_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg',preview_image_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg')
 
 
 @app.route("/callback_TESR", methods=['POST'])
@@ -71,10 +71,10 @@ def handle_message(event):
     elif "menu" in str(event.message.text):
     	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Menu'))
 
-	image_message = ImageSendMessage(original_content_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg',preview_image_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg')
+	#message = ImageSendMessage(original_content_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg',preview_image_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg')
 	#line_bot_api.push_message('ItfoFj89IMTUAR2ERKN1yPxAPZk4UvEC4fperPkGrCg/L6GwTXKR/sOC1KEYyMJppqHG9UdSDvkWNLEWHcC5E5SfptTgPKsgcMgQzFj9nDNTaJlDhv/Xw+0ahLBCWC8nO8sMe6GSGd+dP6fmoFMPNwdB04t89/1O/w1cDnyilFU=', image_message)
 
-	line_bot_api.reply_message(event.reply_token,image_message)
+	line_bot_api.reply_message(event.reply_token,message)
 
 	
     	#--REST API NETPIE OFF LED--
