3a8411bcd6418adf2d6266e5dcc608f681b6ecf0
anoney <ponza.zaza11@gmail.com>
2018-08-16 09:46:35 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -74,8 +74,11 @@ def handle_message(event):
     	original_content_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg',
     	preview_image_url='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg'
     	)
-	line_bot_api.push_message('ItfoFj89IMTUAR2ERKN1yPxAPZk4UvEC4fperPkGrCg/L6GwTXKR/sOC1KEYyMJppqHG9UdSDvkWNLEWHcC5E5SfptTgPKsgcMgQzFj9nDNTaJlDhv/Xw+0ahLBCWC8nO8sMe6GSGd+dP6fmoFMPNwdB04t89/1O/w1cDnyilFU=', image_message)
-
+	#line_bot_api.push_message('ItfoFj89IMTUAR2ERKN1yPxAPZk4UvEC4fperPkGrCg/L6GwTXKR/sOC1KEYyMJppqHG9UdSDvkWNLEWHcC5E5SfptTgPKsgcMgQzFj9nDNTaJlDhv/Xw+0ahLBCWC8nO8sMe6GSGd+dP6fmoFMPNwdB04t89/1O/w1cDnyilFU=', image_message)
+	try:
+		line_bot_api.push_message(event.reply_token,image_message)
+	except LineBotApiError as e:
+	
     	#--REST API NETPIE OFF LED--
     	#r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
 
