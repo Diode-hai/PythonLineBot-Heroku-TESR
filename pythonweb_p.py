0bb723fb70df3b18160a953a7a276810b5717207
anoney <ponza.zaza11@gmail.com>
2018-08-16 13:43:38 +0700

    Update pythonweb_p.py

--- pythonweb_p.py
+++ pythonweb_p.py
@@ -31,7 +31,7 @@ url = 'https://api.netpie.io/topic/' + str(APPID) + str(Topic)
 urlRESTAPI = 'https://api.netpie.io/topic/' + str(APPID) + str(Topic) + '?auth=' + str(KEY) + ':' + str(SECRET)
 #https://api.netpie.io/topic/LineBotRpi/LED_Control?auth=Jk0ej35pLC7TVr1:edWzwTUkzizhlyRamWWq6nF9I
 
-message = ImageSendMessage(original_content_url='https://www.img.in.th/images/04a428c1264fde336fb867412d648f40.jpg',preview_image_url='https://www.img.in.th/images/04a428c1264fde336fb867412d648f40.jpg')
+message = ImageSendMessage(original_content_url='https://image.ibb.co/fx6ibU/hotpotgalaxy240.jpg',preview_image_url='https://image.ibb.co/fx6ibU/hotpotgalaxy240.jpg')
 #---------------------------------------------------
 #@app.route("/")
 #@app.route("/callback")
