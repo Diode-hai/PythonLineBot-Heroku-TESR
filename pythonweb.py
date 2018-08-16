df9c1df19efeff39d62e059a2e994856c803d5a5
anoney <ponza.zaza11@gmail.com>
2018-08-16 10:08:40 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -62,13 +62,13 @@ def handle_message(event):
     	#REST API NETPIE ON LED
     	r = requests.put(url, data = {'':'ON'} , auth=(str(KEY),str(SECRET)))
 		
-    elif "off" in str(event.message.text):
+    if "off" in str(event.message.text): #elif
     	line_bot_api.reply_message(event.reply_token,TextSendMessage(text='OFF LED'))
 
     	#REST API NETPIE OFF LED
     	r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
 	
-    #elif "menu" in str(event.message.text):
+    if "menu" in str(event.message.text):
     	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Menu'))
 
 	image_message = ImageSendMessage(
@@ -83,7 +83,7 @@ def handle_message(event):
     	#--REST API NETPIE OFF LED--
     	#r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
 
-    elif "temp?" in str(event.message.text):
+    if "temp?" in str(event.message.text):
     	#REST API NETPIE read sensor value
     	r = requests.put(url, data = {'':'temp?'} , auth=(str(KEY),str(SECRET)))
     	
