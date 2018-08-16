e8d3f47f040fb6997d07625043f8d8e0aa6d0656
anoney <ponza.zaza11@gmail.com>
2018-08-16 09:13:43 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -69,7 +69,7 @@ def handle_message(event):
     	r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
 	
     elif "menu" in str(event.message.text):
-    	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Menu'))
+    	line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Menu'))
 	#image_message = ImageSendMessage(
     	#original_content_url='https://previews.123rf.com/images/seamartini/seamartini1708/seamartini170800547/84922979-japanese-cuisine-restaurant-menu-template-vector-lunch-offer-for-sweet-syrup-tangerine-tory-kenko-ya.jpg',
     	#preview_image_url='https://previews.123rf.com/images/seamartini/seamartini1708/seamartini170800547/84922979-japanese-cuisine-restaurant-menu-template-vector-lunch-offer-for-sweet-syrup-tangerine-tory-kenko-ya.jpg'
