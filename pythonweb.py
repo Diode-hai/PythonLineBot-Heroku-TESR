6660f8d621f5c47aba24acac92261e5b1b008755
anoney <ponza.zaza11@gmail.com>
2018-08-16 11:12:37 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -70,17 +70,17 @@ def handle_message(event):
     elif "menu" in str(event.message.text):
     	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg'))
 
-	#image_message  = ImageSendMessage(original_content_url='https://www.picz.in.th/images/2018/08/16/BHk2VV.jpg',preview_image_url='https://www.picz.in.th/images/2018/08/16/BHk2VV.jpg')
+	image_message  = ImageSendMessage(original_content_url='https://www.picz.in.th/images/2018/08/16/BHk2VV.jpg',preview_image_url='https://www.picz.in.th/images/2018/08/16/BHk2VV.jpg')
 	#line_bot_api.push_message('ItfoFj89IMTUAR2ERKN1yPxAPZk4UvEC4fperPkGrCg/L6GwTXKR/sOC1KEYyMJppqHG9UdSDvkWNLEWHcC5E5SfptTgPKsgcMgQzFj9nDNTaJlDhv/Xw+0ahLBCWC8nO8sMe6GSGd+dP6fmoFMPNwdB04t89/1O/w1cDnyilFU=', image_message)
 	#line_bot_api.reply_message(event.reply_token,TextSendMessage(text='https://www.picz.in.th/images/2018/08/16/BHYO9V.jpg'))
-	#line_bot_api.reply_message(event.reply_token, image_message )
+	line_bot_api.reply_message(event.reply_token, image_message )
 	
 	#sticker_message = StickerSendMessage(package_id='1',sticker_id='1')
 	#line_bot_api.reply_message(event.reply_token, sticker_message )
 
 	
     	#--REST API NETPIE OFF LED--
-    	#r = requests.put(url, data = {'':'OFF'} , auth=(str(KEY),str(SECRET)))
+    	r = requests.put(url, data = {'':'Menu'} , auth=(str(KEY),str(SECRET)))
 
     elif "temp?" in str(event.message.text):
     	#REST API NETPIE read sensor value
