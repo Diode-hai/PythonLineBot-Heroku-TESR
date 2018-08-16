94ef6956435dd98b0d0cff76b8764b1b33c5288d
anoney <ponza.zaza11@gmail.com>
2018-08-16 16:35:13 +0700

    Update pythonweb_image.py

--- pythonweb_image.py
+++ pythonweb_image.py
@@ -33,7 +33,7 @@ urlRESTAPI = 'https://api.netpie.io/topic/' + str(APPID) + str(Topic) + '?auth='
 
 image_menu = ImageSendMessage(original_content_url='https://www.img.in.th/images/25a44349ac26d45b53b0f4e306449b34.jpg ',preview_image_url='https://image.ibb.co/fx6ibU/hotpotgalaxy240.jpg')
 #message = ImageSendMessage(original_content_url='https://telegram.org/img/t_logo.png',preview_image_url='https://telegram.org/img/t_logo.png')
- message = TemplateSendMessage(alt_text='Confirm template',template=ConfirmTemplate(text='Are you sure?',actions=[PostbackTemplateAction(
+message_yes = TemplateSendMessage(alt_text='Confirm template',template=ConfirmTemplate(text='Are you sure?',actions=[PostbackTemplateAction(
             label='postback',text='postback text',data='action=buy&itemid=1'),MessageTemplateAction(
                 label='message',text='message text')]))
 #---------------------------------------------------
