2ae853673e5efcc187772f631af6fc0f3b1e5038
anoney <ponza.zaza11@gmail.com>
2018-08-17 14:41:47 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -96,7 +96,7 @@ def handle_text_message(event):
         if isinstance(event.source, SourceUser):
             profile = line_bot_api.get_profile(event.source.user_id)
         buttons_template = ButtonsTemplate(
-            title='สวัสดีค่ะ คุณ' + profile.display_name, text='ยินดีตอนรับเข้าสู่ร้านอาหารเวทมนต์ TESR ที่สามารถสั่งงานได้ด้วยระบบ Line Bot ได้ที่เมนูปุ่มด้านล่าง', actions=[
+            title='สวัสดีค่ะ คุณ' + profile.display_name, text='ยินดีตอนรับเข้าสู่ร้านอาหารเวทมนต์ TESR ที่สามารถสั่งงานได้ที่เมนูปุ่มด้านล่าง', actions=[
                 URIAction(label='Go to line.me', uri='https://line.me'),
                 PostbackAction(label='ping', data='ping'),
                 PostbackAction(label='ping with text', data='ping', text='ping'),
